a520d1811f2246ae94713cfc2156a77a263c6edf
tetekate2 <papicaty2@gmail.com>
2018-12-05 16:05:19 +0100

    replace api_key with placeholder

--- src/pybind/mgr/alert/config.py
+++ src/pybind/mgr/alert/config.py
@@ -4,4 +4,4 @@ CONFIG = {
       'test_user' : 'receiver"s_email_kere',
       'mail_username' : 'sender"s_email_here',
       'mail_password' : 'sender"s_password_here'
-      'slack_api' : 'xoxb-487737492849-488179503444-cK5E4qDVGkq2sSY7HBu7vbgX'}
+      'slack_api' : 'api_key_here'}
